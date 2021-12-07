98993756713953d6b14e3594b3551c924774d995
BossNumber1 <dimaang23@gmail.com>
2021-12-07 19:33:02 +0300

    made it possible to transfer the number, but so far only in one direction and for one

--- tuesday_numberOrder/index.js
+++ tuesday_numberOrder/index.js
@@ -1,8 +1,8 @@
 // drag and drop implementation
 
 function dragStart(event) {
-    localStorage.setItem("idTakenCube", event.target.id);
-    localStorage.setItem("nameCube", event.target.dataset.name);
+    localStorage.setItem("idTakenDiv", event.target.id);
+    // localStorage.setItem("categoryDiv", event.target.dataset.name);
 }
 
 function allowDrop(event) {
@@ -11,22 +11,30 @@ function allowDrop(event) {
 
 function drop(e) {
     // получаем id и имя несомого элемента
-    let idTakenCube = localStorage.getItem("idTakenCube");
-    let nameCube = localStorage.getItem("nameCube");
+    let idTakenDiv = localStorage.getItem("idTakenDiv");
+    // let categoryDiv = localStorage.getItem("categoryDiv");
 
     // берем id того элемента, на который положим несомый
     let currentId = e.target.id;
-    let currentName = e.target.dataset.name;
 
-    // меняем картинки местами
-    let orig = document.getElementById(idTakenCube);
-    orig.src = "./pictures/" + currentName + ".svg";
+    // let currentName = e.target.dataset.name;
+
+    // меняем поля местами
+    let orig = document.getElementById(idTakenDiv);
+    // orig.src = "./pictures/" + currentName + ".svg";
     orig.id = currentId;
-    orig.setAttribute("data-name", currentName);
-    orig.parentElement.style.cursor = "default";
+    orig.classList.remove("filledField");
 
-    e.target.src = "./pictures/" + nameCube + ".svg";
-    e.target.id = idTakenCube;
-    e.target.setAttribute("data-name", nameCube);
-    e.target.parentElement.style.cursor = "grab";
+    // orig.setAttribute("data-name", currentName);
+    // orig.parentElement.style.cursor = "default";
+
+    // e.target.src = "./pictures/" + categoryDiv + ".svg";
+    e.target.id = idTakenDiv;
+    e.target.classList.add("filledField");
+    e.target.textContent = orig.textContent;
+    // e.target.setAttribute("data-name", categoryDiv);
+    // e.target.parentElement.style.cursor = "grab";
+
+    // именно - после всех операций - удаляем содержимое изначального места
+    orig.textContent = "";
 }
