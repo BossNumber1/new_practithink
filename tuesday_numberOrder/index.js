f3f5087f7e468b96cbb3fe342f0e6af0a1e0dfb2
BossNumber1 <dimaang23@gmail.com>
2021-12-07 19:09:44 +0300

    prepared the code for improvement

--- tuesday_numberOrder/index.js
+++ tuesday_numberOrder/index.js
@@ -17,7 +17,7 @@ function drop(e) {
     // берем id того элемента, на который положим несомый
     let currentId = e.target.id;
     let currentName = e.target.dataset.name;
-    debugger;
+
     // меняем картинки местами
     let orig = document.getElementById(idTakenCube);
     orig.src = "./pictures/" + currentName + ".svg";
@@ -30,50 +30,3 @@ function drop(e) {
     e.target.setAttribute("data-name", nameCube);
     e.target.parentElement.style.cursor = "grab";
 }
-
-// value checking implementation
-
-let resultChecking = { more: "", smaller: "", equals: "" };
-
-document.getElementById("more").onchange = function (e) {
-    if (e.target.value == "42") {
-        resultChecking.more = "верно";
-    } else {
-        resultChecking.more = "не верно";
-    }
-};
-
-document.getElementById("smaller").onchange = function (e) {
-    if (e.target.value == "37") {
-        resultChecking.smaller = "верно";
-    } else {
-        resultChecking.smaller = "не верно";
-    }
-};
-
-document.getElementById("equals").onchange = function (e) {
-    if (e.target.value == "5") {
-        resultChecking.equals = "верно";
-    } else {
-        resultChecking.equals = "не верно";
-    }
-};
-
-document.getElementById("submit").onclick = function () {
-    if (
-        resultChecking.more !== "" &&
-        resultChecking.smaller !== "" &&
-        resultChecking.equals !== ""
-    ) {
-        alert(
-            "первое поле - " +
-                resultChecking.more +
-                ", второе - " +
-                resultChecking.smaller +
-                ", третье - " +
-                resultChecking.equals
-        );
-    } else {
-        alert("сначала стоит заполнить все поля");
-    }
-};
