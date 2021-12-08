26bdccd7770d84d28854f22068624610f83c42d4
BossNumber1 <dimaang23@gmail.com>
2021-12-08 16:58:38 +0300

    up

--- wednesday_newOrderNumbers/index.js
+++ wednesday_newOrderNumbers/index.js
@@ -28,7 +28,38 @@ function drop(e) {
     orig.id = currentId;
 
     if (classGrandparent === "bottomRow") {
-        orig.style.opacity = "0.5";
+        // orig.style.opacity = "0.5";
+        let pele = orig.parentElement; // получаю родителя
+        let pos = pele.dataset.position; // теперь его позицию
+
+        orig.remove();
+
+        //----
+
+        let objectBeingCreated222 = document.createElement("img");
+        //  objectBeingCreated.src = "./pictures/" + figureId + ".svg";
+        //  objectBeingCreated.id = figureId;
+        //  objectBeingCreated.alt = figureId;
+        //  objectBeingCreated.style.opacity = "1";
+        objectBeingCreated222.style.width = "50px";
+        objectBeingCreated222.style.height = "50px";
+        objectBeingCreated222.style.borderRadius = "50px";
+        objectBeingCreated222.style.background = "lightgray";
+
+        //  objectBeingCreated.setAttribute("data-state", numberSidesFigure);
+        //  objectBeingCreated.setAttribute("data-position", positionFigure);
+
+        let placeSelectedFigure2 =
+            document.getElementsByClassName(classGrandparent)[0].children[pos];
+
+        console.log(
+            "ll 2 =",
+            document.getElementsByClassName(classGrandparent)[0].children[pos]
+        );
+
+        placeSelectedFigure2.appendChild(objectBeingCreated222);
+        //----
+        // orig.style.backgroundColor = "orange";
     } else {
         orig.src = "./pictures/" + currentId + ".svg";
     }
@@ -46,3 +77,23 @@ function drop(e) {
         e.target.style.opacity = "1";
     }
 }
+
+// check implementation
+
+let correctOrder = [23, 24, 25, 26, 27, 28];
+let resultat = [];
+
+document.getElementById("submit").onclick = function () {
+    let topRow = document.getElementsByClassName("topRow");
+    let childrenTopRow = topRow[0].children;
+
+    for (let i = 0; i < childrenTopRow.length; i++) {
+        if (childrenTopRow[i].children[0].id == correctOrder[i]) {
+            resultat.push("верно");
+        } else {
+            resultat.push("нет");
+        }
+    }
+
+    alert(resultat);
+};
