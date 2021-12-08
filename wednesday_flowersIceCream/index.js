fae3dc72caa7324dbf7714717ffdadd98fa42d93
BossNumber1 <dimaang23@gmail.com>
2021-12-08 19:24:03 +0300

    submit implementated

--- wednesday_flowersIceCream/index.js
+++ wednesday_flowersIceCream/index.js
@@ -1,7 +1,21 @@
+let selected = { amountIceCream: 0, amountFlowers: 0 };
+
 document.getElementById("amountIceCream").onchange = function (e) {
-    alert("you decided that we have " + e.target.value + " ice creams");
+    selected.amountIceCream = e.target.value;
 };
 
 document.getElementById("amountFlowers").onchange = function (e) {
-    alert("you decided that we have " + e.target.value + " flowers");
+    selected.amountFlowers = e.target.value;
+};
+
+// submit
+
+document.getElementById("submit").onclick = function () {
+    alert(
+        "you decided that we have " +
+            selected.amountIceCream +
+            " ice creams & " +
+            selected.amountFlowers +
+            " flowers"
+    );
 };
