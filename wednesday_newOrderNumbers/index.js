986a4543a73b082a9d70710c395849959665bcd3
BossNumber1 <dimaang23@gmail.com>
2021-12-08 15:21:46 +0300

    modified

--- wednesday_newOrderNumbers/index.js
+++ wednesday_newOrderNumbers/index.js
@@ -3,7 +3,7 @@
 function dragStart(event) {
     localStorage.setItem("idTakenNumber", event.target.id);
     // localStorage.setItem(
-    //     "classElementGrandparent",
+    //     "classGrandparent",
     //     event.target.parentElement.parentElement.className
     // );
 }
@@ -15,36 +15,31 @@ function allowDrop(event) {
 function drop(e) {
     // получаем id взятого элемента и класс прародителя
     let idTakenNumber = localStorage.getItem("idTakenNumber");
-    // let classElementGrandparent = localStorage.getItem(
-    //     "classElementGrandparent"
-    // );
+    // let classGrandparent = localStorage.getItem("classGrandparent");
+
     // берем id того элемента, на который положим несомый
     let currentId = e.target.id;
+
     // меняем поля местами
     let orig = document.getElementById(idTakenNumber);
-    // orig.id = currentId;
-    // if (classElementGrandparent === "collectionFilledFields") {
-    //     orig.style.background = "#369cb7";
-    // orig.style.opacity = "0.5";
+    orig.id = currentId;
+
+    // if (classGrandparent === "bottomRow") {
+    //     // orig.style.background = "#369cb7";
+    //     // orig.style.opacity = "0.5";
     // } else {
     //     orig.classList.remove("filledField");
     // }
 
-    let elem = e.target;
-    elem.className = "number";
+    let currentElem = document.getElementById(currentId);
+    currentElem.id = idTakenNumber;
+    currentElem.src = "./pictures/" + idTakenNumber + ".svg";
 
-    let objectBeingCreated = document.createElement("img");
-    //   objectBeingCreated.id = "appleInBasket" + positionApple;
+    if (currentId === "square") {
+        e.target.parentElement.className = "number";
+    }
 
-    objectBeingCreated.id = idTakenNumber;
-    objectBeingCreated.src = "./pictures/" + idTakenNumber + ".svg";
-
-    elem.appendChild(objectBeingCreated);
-    // e.target.id = idTakenNumber;
-    // e.target.src = "./pictures/" + idTakenNumber + ".svg";
-
-    // e.target.textContent = orig.textContent;
-    // if (classElementGrandparent !== "collectionFilledFields") {
+    // if (classGrandparent !== "collectionFilledFields") {
     //     e.target.style.opacity = "1";
     // }
 }
