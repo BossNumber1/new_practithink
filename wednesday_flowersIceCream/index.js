9dc80388c9d949dbd211017b1d4663e3dbb269f7
BossNumber1 <dimaang23@gmail.com>
2021-12-08 19:20:21 +0300

    up

---
+++ wednesday_flowersIceCream/index.js
@@ -0,0 +1,7 @@
+document.getElementById("amountIceCream").onchange = function (e) {
+    alert("you decided that we have " + e.target.value + " ice creams");
+};
+
+document.getElementById("amountFlowers").onchange = function (e) {
+    alert("you decided that we have " + e.target.value + " flowers");
+};
