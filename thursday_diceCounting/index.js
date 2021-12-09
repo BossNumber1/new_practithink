f5dcf534e83de6ff0bb5b07e6e1407f28e568ea7
BossNumber1 <dimaang23@gmail.com>
2021-12-09 19:05:29 +0300

    READY!

--- thursday_diceCounting/index.js
+++ thursday_diceCounting/index.js
@@ -1,3 +1,36 @@
+// drag and drop implementation
+
+function dragStart(event) {
+    localStorage.setItem("idTakenCube", event.target.id);
+    localStorage.setItem("nameCube", event.target.dataset.name);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(e) {
+    // получаем id и имя несомого элемента
+    let idTakenCube = localStorage.getItem("idTakenCube");
+    let nameCube = localStorage.getItem("nameCube");
+
+    // берем id того элемента, на который положим несомый
+    let currentId = e.target.id;
+    let currentName = e.target.dataset.name;
+
+    // меняем картинки местами
+    let orig = document.getElementById(idTakenCube);
+    orig.src = "./pictures/" + currentName + ".svg";
+    orig.id = currentId;
+    orig.setAttribute("data-name", currentName);
+    orig.parentElement.style.cursor = "default";
+
+    e.target.src = "./pictures/" + nameCube + ".svg";
+    e.target.id = idTakenCube;
+    e.target.setAttribute("data-name", nameCube);
+    e.target.parentElement.style.cursor = "grab";
+}
+
 // check implementation
 let selectedButton = "";
 
