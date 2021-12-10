09befe2d8802f35e8927e249a7e6486c881762cb
BossNumber1 <dimaang23@gmail.com>
2021-12-10 19:34:11 +0300

    Можно брать для готового шаблона ДНД 🤘

--- friday_arrangeToys/index.js
+++ friday_arrangeToys/index.js
@@ -12,17 +12,14 @@ function drop(e) {
     // забираем данные из хранилища
     let idFigure = localStorage.getItem("idFigure");
 
-    // // получаем класс предка
-    // let grandparentClass = e.target.parentElement.parentElement.className;
-
     // получаем текущий id
     let currentId = e.target.id;
 
-    // начинаем ложить объект в корзину
-    // // получаем картинку для вставки
+    // получаем картинки
     let orig = document.getElementById(idFigure);
     let currentElement = document.getElementById(currentId);
 
+    // начинаем уборку
     if (idFigure != "car" && idFigure != "robot" && idFigure != "pyramid") {
         currentElement.src = "./pictures/" + idFigure.slice(0, -1) + ".svg";
         orig.src = "./pictures/" + idFigure + ".svg";
@@ -33,31 +30,4 @@ function drop(e) {
 
     currentElement.id = idFigure;
     orig.id = currentId;
-
-    // let objectBeingCreated = document.createElement("img");
-    // objectBeingCreated.id = idFigure + "temp";
-    // objectBeingCreated.alt = idFigure;
-    // objectBeingCreated.src = "./pictures/" + idFigure + ".svg";
-    // objectBeingCreated.style.cursor = "grab";
-    // objectBeingCreated.style.marginTop = "5px";
-    // objectBeingCreated.style.marginLeft = "3px";
-
-    // if (grandparentClass !== "objects") {
-    //     document
-    //         .getElementById(currentId)
-    //         .appendChild(document.createElement("div"))
-    //         .appendChild(objectBeingCreated);
-
-    //     orig.src = "./pictures/square.svg";
-    //     orig.style.cursor = "default";
-    // }
-
-    // // делаем возврат на место
-
-    // if (grandparentClass === "objects") {
-    //     e.target.src = "./pictures/" + orig.alt + ".svg";
-    //     e.target.id = orig.alt;
-    //     e.target.style.cursor = "grab";
-    //     orig.remove();
-    // }
 }
