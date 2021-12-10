7a7ffe36f443bb0d67dc4949a77cef4e195b410d
BossNumber1 <dimaang23@gmail.com>
2021-12-10 14:50:56 +0300

    made a return to the place

--- thursday_classifyShapes/index.js
+++ thursday_classifyShapes/index.js
@@ -1,7 +1,7 @@
 // drag and drop implementation
 
-function dragStart(event) {
-    localStorage.setItem("idFigure", event.target.id);
+function dragStart(e) {
+    localStorage.setItem("idFigure", e.target.id);
     // localStorage.setItem("positionAppleInRow", event.target.dataset.position);
     // localStorage.setItem("parentElementIdStart", event.target.parentElement.id);
 }
@@ -10,12 +10,20 @@ function allowDrop(event) {
     event.preventDefault();
 }
 
-function drop() {
+function drop(e) {
     // забираем данные из хранилища
 
     // let parentElementIdStart = localStorage.getItem("parentElementIdStart");
     let idFigure = localStorage.getItem("idFigure");
     // let positionAppleInRow = localStorage.getItem("positionAppleInRow");
+
+    // получаем текущий id
+    let currentId = e.target.id;
+    let grandparentClass = e.target.parentElement.parentElement.className;
+
+    console.log("currentId =", currentId);
+
+    // получаем фигуру...
 
     // начинаем ложить фигуру в корзину
 
@@ -23,17 +31,20 @@ function drop() {
     let orig = document.getElementById(idFigure); // получаем картинку для вставки
 
     let objectBeingCreated = document.createElement("img");
-    objectBeingCreated.id = idFigure;
+    objectBeingCreated.id = idFigure.slice(0, -1);
     objectBeingCreated.src = "./pictures/" + idFigure.slice(0, -1) + ".svg";
     objectBeingCreated.style.cursor = "grab";
     objectBeingCreated.style.marginTop = "5px";
     objectBeingCreated.style.marginLeft = "5px";
 
     //     objectBeingCreated.id = "appleInBasket" + positionAppleInRow;
-    document
-        .getElementsByClassName("placeDroppingFigures")[0]
-        .appendChild(document.createElement("div"))
-        .appendChild(objectBeingCreated);
+
+    if (grandparentClass !== "topRow") {
+        document
+            .getElementsByClassName("placeDroppingFigures")[0]
+            .appendChild(document.createElement("div"))
+            .appendChild(objectBeingCreated);
+    }
 
     // создаём копию и ставим на место оригинала
 
@@ -50,7 +61,17 @@ function drop() {
     //         document.getElementsByClassName("appleInRow")[positionAppleInRow];
     //     newPlaceSelectedApple.appendChild(copyBeingCreated);
     // }
-    // // делаем возврат на место
+
+    // делаем возврат на место
+
+    // console.log("grandpele =", e.target.parentElement.parentElement.className);
+
+    if (grandparentClass === "topRow") {
+        e.target.style.opacity = "1";
+        // document.getElementById("heart").remove();
+        orig.remove();
+        // orig.remove();
+    }
     // if (parentElementIdStart === "appleInBasket" + positionAppleInRow) {
     //     // убираем яблоко из корзины
     //     document.getElementById(parentElementIdStart).remove();
