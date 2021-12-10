317f740f6ab574465c2d24d75bd621b25b414f73
BossNumber1 <dimaang23@gmail.com>
2021-12-10 19:16:37 +0300

    uploaded template

---
+++ friday_arrangeToys/index.js
@@ -0,0 +1,51 @@
+// drag and drop implementation
+
+function dragStart(e) {
+    localStorage.setItem("idFigure", e.target.id);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(e) {
+    // забираем данные из хранилища
+    let idFigure = localStorage.getItem("idFigure");
+
+    // // получаем класс предка
+    // let grandparentClass = e.target.parentElement.parentElement.className;
+
+    // получаем текущий id
+    let currentId = e.target.id;
+
+    // // начинаем ложить объект в корзину
+    // // получаем картинку для вставки
+    // let orig = document.getElementById(idFigure);
+
+    // let objectBeingCreated = document.createElement("img");
+    // objectBeingCreated.id = idFigure + "temp";
+    // objectBeingCreated.alt = idFigure;
+    // objectBeingCreated.src = "./pictures/" + idFigure + ".svg";
+    // objectBeingCreated.style.cursor = "grab";
+    // objectBeingCreated.style.marginTop = "5px";
+    // objectBeingCreated.style.marginLeft = "3px";
+
+    // if (grandparentClass !== "objects") {
+    //     document
+    //         .getElementById(currentId)
+    //         .appendChild(document.createElement("div"))
+    //         .appendChild(objectBeingCreated);
+
+    //     orig.src = "./pictures/square.svg";
+    //     orig.style.cursor = "default";
+    // }
+
+    // // делаем возврат на место
+
+    // if (grandparentClass === "objects") {
+    //     e.target.src = "./pictures/" + orig.alt + ".svg";
+    //     e.target.id = orig.alt;
+    //     e.target.style.cursor = "grab";
+    //     orig.remove();
+    // }
+}
