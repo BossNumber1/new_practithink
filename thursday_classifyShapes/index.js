a9a9c46f0e642f3603e7b1f5d6150a0c6a97b033
BossNumber1 <dimaang23@gmail.com>
2021-12-10 14:27:08 +0300

    made it possible to insert a shape into the basket

---
+++ thursday_classifyShapes/index.js
@@ -0,0 +1,56 @@
+// drag and drop implementation
+
+function dragStart(event) {
+    localStorage.setItem("idFigure", event.target.id);
+    // localStorage.setItem("positionAppleInRow", event.target.dataset.position);
+    // localStorage.setItem("parentElementIdStart", event.target.parentElement.id);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop() {
+    // забираем данные из хранилища
+
+    // let parentElementIdStart = localStorage.getItem("parentElementIdStart");
+    let idFigure = localStorage.getItem("idFigure");
+
+    // let positionAppleInRow = localStorage.getItem("positionAppleInRow");
+    // // начинаем ложить яблоко в корзину
+    // if (parentElementIdStart !== "appleInBasket" + positionAppleInRow) {
+    // let selectedFigure = document.getElementById(idFigure); // получаем картинку для вставки
+
+    let objectBeingCreated = document.createElement("div");
+    objectBeingCreated.style.cursor = "grab";
+
+    let childImplementableObject = document.createElement("img");
+    childImplementableObject.id = idFigure;
+    childImplementableObject.src =
+        "./pictures/" + idFigure.slice(0, -1) + ".svg";
+    //     objectBeingCreated.id = "appleInBasket" + positionAppleInRow;
+    document
+        .getElementsByClassName("placeDroppingFigures")[0]
+        .appendChild(objectBeingCreated)
+        .appendChild(childImplementableObject);
+    //     // создаём копию и ставим на место оригинала
+    //     let copyBeingCreated = document.createElement("img");
+    //     copyBeingCreated.src = "./pictures/apple.svg";
+    //     copyBeingCreated.id = idFigure;
+    //     copyBeingCreated.setAttribute("data-position", positionAppleInRow);
+    //     copyBeingCreated.style.opacity = "0.5";
+    //     copyBeingCreated.style.cursor = "default";
+    //     let newPlaceSelectedApple =
+    //         document.getElementsByClassName("appleInRow")[positionAppleInRow];
+    //     newPlaceSelectedApple.appendChild(copyBeingCreated);
+    // }
+    // // делаем возврат на место
+    // if (parentElementIdStart === "appleInBasket" + positionAppleInRow) {
+    //     // убираем яблоко из корзины
+    //     document.getElementById(parentElementIdStart).remove();
+    //     // возвращаем прозрачность положенному яблоку
+    //     let returnedApple =
+    //         document.getElementsByClassName("appleInRow")[positionAppleInRow];
+    //     returnedApple.children[0].style.opacity = "1";
+    // }
+}
