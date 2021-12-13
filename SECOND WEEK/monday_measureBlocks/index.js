50882b5dbd614a2af65bf4bfad259db4df682949
BossNumber1 <dimaang23@gmail.com>
2021-12-13 18:48:17 +0300

    refactored

--- SECOND WEEK/monday_measureBlocks/index.js
+++ SECOND WEEK/monday_measureBlocks/index.js
@@ -31,54 +31,7 @@ function drop(e) {
     e.target.parentElement.style.cursor = "grab";
 }
 
-// value checking implementation
-
-// let resultChecking = { more: "", smaller: "", equals: "" };
-
-// document.getElementById("more").onchange = function (e) {
-//     if (e.target.value == "42") {
-//         resultChecking.more = "верно";
-//     } else {
-//         resultChecking.more = "не верно";
-//     }
-// };
-
-// document.getElementById("smaller").onchange = function (e) {
-//     if (e.target.value == "37") {
-//         resultChecking.smaller = "верно";
-//     } else {
-//         resultChecking.smaller = "не верно";
-//     }
-// };
-
-// document.getElementById("equals").onchange = function (e) {
-//     if (e.target.value == "5") {
-//         resultChecking.equals = "верно";
-//     } else {
-//         resultChecking.equals = "не верно";
-//     }
-// };
-
-// document.getElementById("submit").onclick = function () {
-//     if (
-//         resultChecking.more !== "" &&
-//         resultChecking.smaller !== "" &&
-//         resultChecking.equals !== ""
-//     ) {
-//         alert(
-//             "первое поле - " +
-//                 resultChecking.more +
-//                 ", второе - " +
-//                 resultChecking.smaller +
-//                 ", третье - " +
-//                 resultChecking.equals
-//         );
-//     } else {
-//         alert("сначала стоит заполнить все поля");
-//     }
-// };
-
-///------------------------------
+// check implementation
 
 document.getElementById("selectBrush").onclick = function () {
     document.getElementById("selectBrush").style.backgroundColor = "#369CB7";
