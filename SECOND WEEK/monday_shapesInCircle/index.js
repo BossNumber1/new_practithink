1521ba5847dd88466a45aa7dba2b26cc43188f3e
BossNumber1 <dimaang23@gmail.com>
2021-12-13 20:41:16 +0300

    did leaving pictures anywhere

--- SECOND WEEK/monday_shapesInCircle/index.js
+++ SECOND WEEK/monday_shapesInCircle/index.js
@@ -1,72 +1,94 @@
 // drag and drop implementation
 
 function dragStart(event) {
-    localStorage.setItem("idOrig", event.target.id);
+    localStorage.setItem("idOriginal", event.target.id);
 }
 
 function allowDrop(event) {
     event.preventDefault();
 }
 
-function drop(e) {
+function drop2(e) {
     // получаем имя и id взятого элемента
-    let idOrig = localStorage.getItem("idOrig");
-    let nameObjectOrig = idOrig.slice(0, -1);
+    let idOrig = localStorage.getItem("idOriginal");
 
     // получаем имя и id, на который кладём элемент
-    let currentId = e.target.id;
-    let nameObjectCurrent = currentId.slice(0, -1);
+    let currentId;
+
+    if (e.target.id !== "flowersPlace" && e.target.id !== "redColorPlace") {
+        currentId = e.target.id;
+    } else {
+        currentId = "emptyPlace";
+    }
 
     // получаем объекты
     let orig = document.getElementById(idOrig);
     let currentElement = document.getElementById(currentId);
 
     // меняем картинки местами
-    currentElement.src = "./pictures/" + nameObjectOrig + ".svg";
-    currentElement.style.opacity = "1";
-    currentElement.style.cursor = "grab";
+    currentElement.src = "./pictures/" + idOrig + ".svg";
 
-    if (
-        orig.parentElement.parentElement.className === "topRow" ||
-        orig.parentElement.parentElement.className === "bottomRow"
-    ) {
-        orig.style.opacity = "0.5";
-    } else {
-        orig.src = "./pictures/" + nameObjectCurrent + ".svg";
-    }
+    // if (
+    //     orig.parentElement.parentElement.className === "topRow" ||
+    //     orig.parentElement.parentElement.className === "bottomRow"
+    // ) {
+    //     orig.style.opacity = "0.5";
+    // } else {
+    orig.src = "./pictures/" + currentId + ".svg";
+    // }
 
     // меняем id местами
     currentElement.id = idOrig;
     orig.id = currentId;
+}
 
-    // меняем вид курсора
-    orig.style.cursor = "default";
+function drop(e) {
+    // забираем данные из хранилища
+    let idFigure = localStorage.getItem("idOriginal");
+
+    // получаем текущий id
+    let currentId = e.target.id;
+
+    // получаем картинки
+    let orig = document.getElementById(idFigure);
+    let currentElement = document.getElementById(currentId);
+
+    // начинаем уборку
+    if (currentId === "flowersPlace" || currentId === "redColorPlace") {
+        // добавляем объект в корзину
+
+        let tray = document.createElement("div");
+        tray.style.height = "50px";
+        tray.style.width = "50px";
+        tray.style.position = "absolute";
+        tray.style.paddingTop = e.clientY - 270 + "px";
+        tray.style.paddingLeft = e.clientX - 430 + "px";
+
+        let objectBeingCreated = document.createElement("img");
+        objectBeingCreated.src = "./pictures/" + idFigure + ".svg";
+        objectBeingCreated.id = idFigure;
+
+        currentElement.appendChild(tray).appendChild(objectBeingCreated);
+
+        // заменяем место объекта на квадрат
+        orig.src = "./pictures/emptyPlace.svg";
+        orig.style.cursor = "default";
+        orig.id = "emptyPlace";
+    } else {
+        currentElement.src = "./pictures/" + idFigure + ".svg";
+        currentElement.style.cursor = "grab";
+        currentElement.id = idFigure;
+
+        orig.remove();
+    }
 }
 
 // check implementation
 
-let selectBtn;
+// document.getElementById("flowersPlace").onclick = function () {
+//     alert("clicked to flowers area");
+// };
 
-document.getElementById("selectBrush").onclick = function () {
-    document.getElementById("selectBrush").style.backgroundColor = "#369CB7";
-    document.getElementById("selectBrush").style.color = "white";
-
-    selectBtn = "right";
-
-    document.getElementById("selectPencil").style.backgroundColor = "white";
-    document.getElementById("selectPencil").style.color = "black";
-};
-
-document.getElementById("selectPencil").onclick = function () {
-    document.getElementById("selectPencil").style.backgroundColor = "#369CB7";
-    document.getElementById("selectPencil").style.color = "white";
-
-    selectBtn = "wrong";
-
-    document.getElementById("selectBrush").style.backgroundColor = "white";
-    document.getElementById("selectBrush").style.color = "black";
-};
-
-document.getElementById("submit").onclick = function () {
-    alert("you made the " + selectBtn + " choice");
-};
+// document.getElementById("redColorPlace").onclick = function () {
+//     alert("clicked to red color area");
+// };
