8864031eabd0f0cee4132b6de049bf6229c6979c
BossNumber1 <dimaang23@gmail.com>
2021-12-14 20:50:01 +0300

    added check implementation

--- SECOND WEEK/tuesday_heavyObject/index.js
+++ SECOND WEEK/tuesday_heavyObject/index.js
@@ -4,7 +4,7 @@ let selectBtn;
 
 let elements = document.getElementsByClassName("row");
 
-document.getElementById("balloon").onclick = function (e) {
+document.getElementById("balloon").onclick = function () {
     document.getElementById("balloon").classList.add("selectedPicture");
 
     selectBtn = "wrong";
@@ -15,7 +15,7 @@ document.getElementById("balloon").onclick = function (e) {
     document.getElementById("car").classList.remove("selectedPicture");
 };
 
-document.getElementById("book").onclick = function (e) {
+document.getElementById("book").onclick = function () {
     document.getElementById("book").classList.add("selectedPicture");
 
     selectBtn = "wrong";
@@ -26,7 +26,7 @@ document.getElementById("book").onclick = function (e) {
     document.getElementById("car").classList.remove("selectedPicture");
 };
 
-document.getElementById("truck").onclick = function (e) {
+document.getElementById("truck").onclick = function () {
     document.getElementById("truck").classList.add("selectedPicture");
 
     selectBtn = "wrong";
@@ -37,7 +37,7 @@ document.getElementById("truck").onclick = function (e) {
     document.getElementById("car").classList.remove("selectedPicture");
 };
 
-document.getElementById("feather").onclick = function (e) {
+document.getElementById("feather").onclick = function () {
     document.getElementById("feather").classList.add("selectedPicture");
 
     selectBtn = "wrong";
@@ -48,13 +48,17 @@ document.getElementById("feather").onclick = function (e) {
     document.getElementById("car").classList.remove("selectedPicture");
 };
 
-document.getElementById("car").onclick = function (e) {
+document.getElementById("car").onclick = function () {
     document.getElementById("car").classList.add("selectedPicture");
 
-    selectBtn = "wrong";
+    selectBtn = "right";
 
     document.getElementById("balloon").classList.remove("selectedPicture");
     document.getElementById("book").classList.remove("selectedPicture");
     document.getElementById("truck").classList.remove("selectedPicture");
     document.getElementById("feather").classList.remove("selectedPicture");
+};
+
+document.getElementById("submit").onclick = function () {
+    alert("you have selected the " + selectBtn + " object");
 };
